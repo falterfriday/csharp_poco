f3fbbbbb02ff5cfbb9659d3d98762e0a9aa6c8bd
Patrick Todd <patrick.a.todd@gmail.com>
2016-12-12 18:09:02 -0500

    created new class and folder

---
+++ poco/Model/Produce.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poco.Model
+{
+    class Fruit
+    {
+        //don't have to declare a contructor method
+        //one is provided for you "no-arguments contructor"
+
+        //this is a no-arguments contructor
+        public Fruit()
+        {
+            //see... no arguments
+        }
+
+        //this is a custom constructor
+        public Fruit(string name, double weight, int quantity)
+        {
+            Name = name;
+            Weight = weight;
+            Quantity = quantity;
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private double weight;
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        public int Quantity { get; set; }
+
+        //tells the compiler that you want to use
+        //this version on the ToString method
+        public override string ToString()
+        {
+            return Name + "(" + Weight + "oz) : " + Quantity;
+        }
+    }
+
+    class Vegetable
+    {
+        //no-args contructor
+        public Vegetable()
+        {
+        }
+
+        //custom constructor
+        public Vegetable(string name, double weight, int quantity)
+        {
+            Name = name;
+            Weight = weight;
+            Quantity = quantity;
+        }
+
+        //encapsulated data
+        private string name;
+        public string Name
+        {
+            get { return name.ToUpper(); }
+            set { name = value; }
+        }
+        private double weight;
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        public int Quantity { get; set; }
+
+        //tells the compiler that you want to use
+        //this version on the ToString method
+        public override string ToString()
+        {
+            return Name + "(" + Weight + "oz) : " + Quantity;
+        }
+    }
+}
--- poco/Program.cs
+++ poco/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
+﻿using poco.Model;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace poco //plain-old class objects
 {
@@ -27,7 +25,7 @@ namespace poco //plain-old class objects
             f2.Quantity = 30;
             produce.Add(f2);
 
-            //this is the slightly easier way
+            //this is the slightly different way
             produce.Add(new Vegetable());
             ((Vegetable)produce[2]).Name = "Carrot";
             ((Vegetable)produce[2]).Weight = 1.2;
@@ -39,87 +37,6 @@ namespace poco //plain-old class objects
             {
                 Console.WriteLine(item);
             }
-        }
-    }
-
-    class Fruit
-    {
-        //don't have to declare a contructor method
-        //one is provided for you "no-arguments contructor"
-
-        //this is a no-arguments contructor
-        public Fruit()
-        {
-            //see... no arguments
-        }
-
-        //this is a custom constructor
-        public Fruit(string name, double weight, int quantity)
-        {
-            Name = name;
-            Weight = weight;
-            Quantity = quantity;
-        }
-
-        private string name;
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-        private double weight;
-        public double Weight
-        {
-            get { return weight; }
-            set { weight = value; }
-        }
-
-        public int Quantity { get; set; }
-        
-        //tells the compiler that you want to use
-        //this version on the ToString method
-        public override string ToString()
-        {
-            return Name + "(" + Weight + "oz) : " + Quantity;
-        }
-    }
-
-    class Vegetable
-    {
-        //no-args contructor
-        public Vegetable()
-        {
-        }
-
-        //custom constructor
-        public Vegetable(string name, double weight, int quantity)
-        {
-            Name = name;
-            Weight = weight;
-            Quantity = quantity;
-        }
-
-        //encapsulated data
-        private string name;
-        public string Name
-        {
-            get { return name.ToUpper(); }
-            set { name = value; }
-        }
-        private double weight;
-        public double Weight
-        {
-            get { return weight; }
-            set { weight = value; }
-        }
-
-        public int Quantity { get; set; }
-
-        //tells the compiler that you want to use
-        //this version on the ToString method
-        public override string ToString()
-        {
-            return Name + "(" + Weight + "oz) : " + Quantity;
         }
     }
 }
