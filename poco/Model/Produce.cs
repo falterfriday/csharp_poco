3eeab5edc84294df15cfada80a5cda8611bdfaf6
Patrick Todd <patrick.a.todd@gmail.com>
2016-12-12 18:19:44 -0500

    set produce as superclass that is inherited by fruit,vegetable

--- poco/Model/Produce.cs
+++ poco/Model/Produce.cs
@@ -6,19 +6,19 @@ using System.Threading.Tasks;
 
 namespace poco.Model
 {
-    class Fruit
+    class Produce
     {
         //don't have to declare a contructor method
         //one is provided for you "no-arguments contructor"
 
         //this is a no-arguments contructor
-        public Fruit()
+        public Produce()
         {
             //see... no arguments
         }
 
         //this is a custom constructor
-        public Fruit(string name, double weight, int quantity)
+        public Produce(string name, double weight, int quantity)
         {
             Name = name;
             Weight = weight;
@@ -47,8 +47,24 @@ namespace poco.Model
             return Name + "(" + Weight + "oz) : " + Quantity;
         }
     }
+    
+    //declaring Fruit as a subclass to Produce
+    class Fruit : Produce
+    {
+        //no-args constructor
+        public Fruit()
+        {
+        }
 
-    class Vegetable
+        //allows fruit to inherit everything from Produce
+        public Fruit(string name, double weight, int quantity) :
+            base(name, weight, quantity)
+        {
+        }
+    }
+
+    //declaring Vegetable as a subclass to Produce
+    class Vegetable : Produce
     {
         //no-args contructor
         public Vegetable()
@@ -56,34 +72,10 @@ namespace poco.Model
         }
 
         //custom constructor
-        public Vegetable(string name, double weight, int quantity)
+        public Vegetable(string name, double weight, int quantity) :
+            base(name, weight, quantity)
         {
-            Name = name;
-            Weight = weight;
-            Quantity = quantity;
         }
-
-        //encapsulated data
-        private string name;
-        public string Name
-        {
-            get { return name.ToUpper(); }
-            set { name = value; }
-        }
-        private double weight;
-        public double Weight
-        {
-            get { return weight; }
-            set { weight = value; }
-        }
-
-        public int Quantity { get; set; }
-
-        //tells the compiler that you want to use
-        //this version on the ToString method
-        public override string ToString()
-        {
-            return Name + "(" + Weight + "oz) : " + Quantity;
-        }
+        
     }
 }
